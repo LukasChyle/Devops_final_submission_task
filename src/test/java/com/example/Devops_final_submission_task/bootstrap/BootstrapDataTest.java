382a4f8b77ba8563618f40266a28a5f52bb252ea
Lukas Chyle <lukas_chyle@hotmail.com>
2023-10-04 13:15:31 +0200

    test push

--- src/test/java/com/example/Devops_final_submission_task/bootstrap/BootstrapDataTest.java
+++ src/test/java/com/example/Devops_final_submission_task/bootstrap/BootstrapDataTest.java
@@ -23,12 +23,12 @@ class BootstrapDataTest {
     @InjectMocks
     private BootstrapData bootstrapData;
 
-    // test that the menu is 7 day...
+    // test that the menu is 7 day.
     @Test
     void testMenuReader() {
         when(menuRepo.getMenuMap()).thenReturn(mockMenuMap);
         bootstrapData.menuReader();
-        verify(mockMenuMap, times(7)).put(anyInt(), anyString());
+        verify(mockMenuMap, times(8)).put(anyInt(), anyString());
     }
 }
 
